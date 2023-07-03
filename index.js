76669e8ab99485aa09a1535d2ed8434dc8436af4
Maxim Bernatsky <maxsateon@yandex.ru>
2023-07-03 23:27:23 +0300

    feat: create login

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import { validationResult } from 'express-validator';
 
 import { registerValidation } from './validations/auth.js';
 import User from './models/User.js';
+import UserSchema from './models/User.js';
 
 mongoose
   .connect('mongodb://127.0.0.1/blogdb')
@@ -51,6 +52,45 @@ app.post('/auth/register', registerValidation, async (req, res) => {
     console.log(e);
     res.status(500).json({
       message: 'Не удалось зарегистрироваться',
+    });
+  }
+});
+
+app.post('/auth/login', async (req, res) => {
+  try {
+    const user = await UserSchema.findOne({ email: req.body.email });
+
+    if (!user) {
+      return res.status(404).json({
+        message: 'Пользователь не найден',
+      });
+    }
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: 'Неверный логин или пароль',
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      'secret-key',
+      { expiresIn: '30d' }
+    );
+
+    const { passwordHash, ...userData } = user._doc;
+
+    res.json({ ...userData, token });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: 'Не удалось авторизоваться',
     });
   }
 });
